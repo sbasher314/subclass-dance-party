3ad18db61b49daab1ec77dd06c00d13419f05d4a
Steven Basher <sbasher314@gmail.com>
2021-01-23 11:56:38 -0500

    Added (incomplete) additional tests file

---
+++ spec/additionalSpec.js
@@ -0,0 +1,48 @@
+describe('Addtional Specs', function() {
+  //  we want to create a number of dancers --
+  //  perhaps a couple of each type?
+
+  //  verify constructor of each dancer is the appropriate constructor -
+  //    blinkyDancer.constructor === BlinkyDancer and so forth
+
+  //  ... Then call n-closest dancers. probably n=2
+  //  make sure that it does indeed return the correct dancers, and does not reference itself from itself (the returned element should not contain the calling element)
+
+  //  also, make sure our 'delete' method works -- dancer.mouseOver()
+  //    should change window.dancers[] length
+  //    should release reference to previous dancer
+  //    n-closest dancers should reflect the changes as well
+  var rotatingDancer, secondDancer, thirdDancer, clock;
+  var timeBetweenSteps = 200;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    rotatingDancer = makeRotatingDancer(10, 20, timeBetweenSteps);
+    window.dancers.push(rotatingDancer);
+    clock.tick(timeBetweenSteps);
+    secondDancer = makeMovingDancer(10, 10, timeBetweenSteps);
+    window.dancers.push(secondDancer);
+    clock.tick(timeBetweenSteps);
+    thirdDancer = makeDancer(20, 20, timeBetweenSteps);
+    window.dancers.push(thirdDancer);
+  });
+
+  /*it('should set color ', function() {
+    /*sinon.spy(rotatingDancer.$node, 'addClass');
+    expect(rotatingDancer.$node.addClass.called).to.be.false;
+    //rotatingDancer.step();
+    expect(rotatingDancer.$node.addClass.called).to.be.true;
+  });
+  */
+
+  it('should delete dancer after mouseover', function() {
+    expect(window.dancers.length).to.equal(3);
+    thirdDancer.$node.mouseover();
+    expect(window.dancers.length).to.equal(2);
+  });
+
+  it('should call step at least once per second', function() {
+    sinon.spy(rotatingDancer, 'step');
+  });
+
+});
